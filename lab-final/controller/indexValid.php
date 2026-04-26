33b142025329ccd0bc4ded082b2c8deb5543868b
Shakil-AIUB <shakilshahria23@gmail.com>
2026-04-26 18:58:57 +0600

    Refactor indexValid.php to remove password and add new fields
    Removed password handling and added new fields for website, comment, and gender.

--- lab-final/controller/indexValid.php
+++ lab-final/controller/indexValid.php
@@ -2,21 +2,27 @@
 <?php 
 
 $name="";
-$password ="";
-$validpassword = "";
-$email = "";
+$email="";
+$website="";
+$comment="";
+$gender="";
+
 
 if($_SERVER["REQUEST_METHOD"]=="POST")
     {
         $name = $_POST["name"];
-        $password= $_POST["password"];
         $email= $_POST["email"];
+        $website= $_POST["website"];
+        $comment= $_POST["comment"];
+        $gender= $_POST["gender"];
+
 
         $name = $_REQUEST["name"];
-        $password= $_REQUEST["password"];
         $email= $_REQUEST["email"];
-
-
+        $website= $_REQUEST["website"];
+        $comment= $_REQUEST["comment"];
+        $gender= $_REQUEST["gender"];
+        
         if(!empty($name) && strlen($name)>=5)
             {
                 echo "User Name: ".$name;
@@ -25,17 +31,6 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
                 echo "UserName must be greater than 5 char";
             }
 
-
-            if(strlen($password)>4)
-                {
-                   $validpassword = $password;
-                   echo " Password: ".$validpassword;
-                }
-                else{
-                    
-                    $validpassword = " Password Must be 4 Digit minimum";
-                    
-                }
             if(!empty($email) && preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/",$email))
                 {
                     echo " Email: ".$email;
@@ -43,8 +38,6 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
                 else{
                     echo " Invalid Email";
                 }
-
-
 
 
     }
